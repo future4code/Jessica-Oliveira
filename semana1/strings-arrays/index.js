0ecf7be558fe05d532b81009b47ba99b61bc7a0c
Jessica de Oliveira <t.jessica.oliveira@gmail.com>
2021-08-11 15:58:36 -0300

    Exercícios aula 05 resolvidos

---
+++ semana1/strings-arrays/index.js
@@ -0,0 +1,56 @@
+// Exercícios - Interpretação de código
+// 1) 
+// a. undefined
+// b. null
+// c. 11
+// d. 3
+// e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// f. 9
+
+// 2) SUBI NUM ONIBUS EM MIRROCOS 27
+
+// Exercícios - Escrita de código
+// 1.
+// const nomeDoUsuario = prompt("Qual o seu nome?")
+// const emailDoUsuario = prompt("Insira aqui o seu email.")
+
+// console.log(`O email ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`)
+
+// 2.
+/*const comidasPreferidas = ["lasanha", "churrasco", "hamburguer", "sushi", "pizza"]
+console.log(comidasPreferidas)
+
+console.log(`Essas são minhas comidas preferidas:
+${comidasPreferidas[0]}
+${comidasPreferidas[1]}
+${comidasPreferidas[2]}
+${comidasPreferidas[3]}
+${comidasPreferidas[4]}`)
+
+// const comidaUsuario = prompt("Insira sua comida preferida.")
+// comidasPreferidas.splice(1, 1, comidaUsuario)
+// console.log(comidasPreferidas)*/
+
+// 3. 
+// const listaDeTarefas = [prompt("Digite uma tarefa que você precisa fazer hoje."), prompt("Digite mais uma tarefa aqui."), prompt("Insira uma outra tarefa que você precisa fazer hoje")]
+// console.log(listaDeTarefas)
+
+// listaDeTarefas.splice(prompt("Digite aqui um dos números: 0, 1 ou 2"),1)
+// console.log(listaDeTarefas)
+
+//DESAFIO
+// 1. 
+// let frase = prompt("Insira aqui uma frase")
+
+// const listaPalavras = frase.split(" ")
+// console.log(listaPalavras)
+
+// 2.
+
+// const listaDeFrutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+// const indexAbacaxi = (listaDeFrutas.indexOf("Abacaxi"))
+// console.log(indexAbacaxi, listaDeFrutas.length)
+
+
+
+
